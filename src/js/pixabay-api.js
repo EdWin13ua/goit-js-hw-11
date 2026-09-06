9a125f860f6d7baf3f8f53f45bed764b81c8e065
EdWin <ivan.sinyutin@gmail.com>
2026-09-06 14:58:26 +0300

    fix2

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const API_KEY = '57459498-0d7fb26521303247ee673eaca';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export function getImagesByQuery(query) {
+  return axios
+    .get(BASE_URL, {
+      params: {
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+      },
+    })
+    .then(response => response.data);
+}
---
+++ src/js/render-functions.js
@@ -0,0 +1,61 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryContainer = document.querySelector('.gallery');
+const loaderElement = document.querySelector('.loader');
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+export function createGallery(images) {
+  const markup = images
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `
+    <li class="gallery-item">
+      <a class="gallery-link" href="${largeImageURL}">
+        <img
+          class="gallery-image"
+          src="${webformatURL}"
+          alt="${tags}"
+        />
+      </a>
+      <div class="info">
+        <p class="info-item"><b>Likes</b> <span>${likes}</span></p>
+        <p class="info-item"><b>Views</b> <span>${views}</span></p>
+        <p class="info-item"><b>Comments</b> <span>${comments}</span></p>
+        <p class="info-item"><b>Downloads</b> <span>${downloads}</span></p>
+      </div>
+    </li>
+  `
+    )
+    .join('');
+
+  galleryContainer.innerHTML = markup;
+  lightbox.refresh();
+}
+
+export function clearGallery() {
+  galleryContainer.innerHTML = '';
+}
+
+export function showLoader() {
+  if (loaderElement) {
+    loaderElement.classList.remove('hidden');
+  }
+}
+
+export function hideLoader() {
+  if (loaderElement) {
+    loaderElement.classList.add('hidden');
+  }
+}
